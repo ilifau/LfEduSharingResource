fcb0543c9c7337ac571b62fc9e1ea97ae3794baf
Fred Neumann <fred.neumann@fau.de>
2022-04-24 22:16:19 +0200

    initial import

---
+++ plugin.php
@@ -0,0 +1,22 @@
+<?php
+ 
+// alphanumerical ID of the plugin; never change this
+$id = "xesr";
+ 
+// code version; must be changed for all code changes
+$version = "0.2.0";
+ 
+// ilias min and max version; must always reflect the versions that should
+// run with the plugin
+$ilias_min_version = "5.1.0";
+$ilias_max_version = "7.99";
+ 
+// optional, but useful: Add one or more responsible persons and a contact email
+$responsible = "metaVentis GmbH | https://metaventis.com";
+$responsible_mail = "info@metaventis.com";
+
+$supports_export = false;
+
+// Enable learning progress
+$learning_progress = false;
+?>
